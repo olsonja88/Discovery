d5b7a664948fd29bed75086f9072b74a01245c71
jaolson88 <jaolson190@gmail.com>
2023-04-12 14:49:52 -0500

    Changed stretch mode to canvas_items.  Created new logic handler Node2D and attached Logic.cs

--- logic.cs
+++ logic.cs
@@ -1,17 +1,22 @@
 using Godot;
 using System;
 
-public partial class logic : Node
+public partial class logic : Node2D
 {
+    // New vector for game window screenSize
+    public Vector2 screenSize;
+
     public override void _Ready()
     {
         // Get player node on start-up
         var playerInit = GetNode<CharacterBody2D>("Player");
+        // Set screenSize to Screen Size
+        screenSize = GetViewportRect().Size;
     }
 
-    public override void _Process()
+    public override void _Process(double Delta)
     {
-
+        
     }
 
     private void HandleDeath()
