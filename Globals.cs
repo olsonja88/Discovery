b2a9d2ecf3f197ee47f8e220bbd96b591936b884
jaolson88 <jaolson190@gmail.com>
2023-04-12 15:45:11 -0500

    Refactor[1]: Added getter and setter methods for playerLives in Globals.cs, removed init value

--- Globals.cs
+++ Globals.cs
@@ -3,4 +3,16 @@ using System;
 
 public partial class Globals : Node
 {
+    // Number of player lives
+    public int playerLives;
+
+    private int GetPlayerLives()
+    {
+        return playerLives;
+    }
+
+    private void SetPlayerLives(int num)
+    {
+        playerLives = num;
+    }
 }
