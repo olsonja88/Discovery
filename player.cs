4c8c926aaae339470e21332a971113a09d6e33db
jaolson88 <jaolson190@gmail.com>
2023-04-13 12:55:35 -0500

    Refactor[1]: Moved jump code from _PhysicsProcess() to HandleJumping()

--- player.cs
+++ player.cs
@@ -57,57 +57,7 @@ public partial class player : CharacterBody2D
             HandleHorizontalAirMovement();
         }
  
-        // If not jumping currently
-        if (!isJumping)
-        {
-            // Releasing space button allows jumping again
-            if (Input.IsActionJustReleased("jump"))
-            {
-                jumpCD = false;
-            } else if (Input.IsActionPressed("jump") && IsOnFloor() && !jumpCD)
-            {
-                // Jumping
-                isJumping = true;
-                jumpCD = true;
-                // Start JumpTimer
-                GetNode<Timer>("JumpTimer").Start();
-                // Go up fast
-                motion.Y = (jumpForce * (float)delta) * -23;
-            }
-            // Else you can't jump
-            else
-            {
-                // Gravity is applied
-                motion.Y += (gravity * (float)delta) * 2;
-            }
-        }
-
-        // If jumping
-        if (isJumping)
-        {
-            // If jump is pressed and motion.Y is above jumpCeil velocity
-            if (Input.IsActionPressed("jump") && motion.Y > jumpCeil)
-            {
-                // Go up
-                motion.Y -= jumpForce * (float)delta;
-            }
-            // Else if jump is released
-            else if (Input.IsActionJustReleased("jump"))
-            {
-                // Turn off jumping
-                isJumping = false;
-                // jumpCD off
-                jumpCD = false;
-            }
-            // Else jumping and below jumpCeil
-            else
-            {
-                // Turn off jumping
-                isJumping = false;
-                // Gravity is applied
-                motion.Y += (gravity * (float)delta) * 2;
-            }
-        }// End if (isJumping)
+        
 
         // Setting built-in Velocity to motion
         Velocity = motion;
@@ -159,7 +109,58 @@ public partial class player : CharacterBody2D
 
     private void HandleJumping()
     {
+        // If not jumping currently
+        if (!isJumping)
+        {
+            // Releasing space button allows jumping again
+            if (Input.IsActionJustReleased("jump"))
+            {
+                jumpCD = false;
+            }
+            else if (Input.IsActionPressed("jump") && IsOnFloor() && !jumpCD)
+            {
+                // Jumping
+                isJumping = true;
+                jumpCD = true;
+                // Start JumpTimer
+                GetNode<Timer>("JumpTimer").Start();
+                // Go up fast
+                motion.Y = (jumpForce * (float)delta) * -23;
+            }
+            // Else you can't jump
+            else
+            {
+                // Gravity is applied
+                motion.Y += (gravity * (float)delta) * 2;
+            }
+        }
 
+        // If jumping
+        if (isJumping)
+        {
+            // If jump is pressed and motion.Y is above jumpCeil velocity
+            if (Input.IsActionPressed("jump") && motion.Y > jumpCeil)
+            {
+                // Go up
+                motion.Y -= jumpForce * (float)delta;
+            }
+            // Else if jump is released
+            else if (Input.IsActionJustReleased("jump"))
+            {
+                // Turn off jumping
+                isJumping = false;
+                // jumpCD off
+                jumpCD = false;
+            }
+            // Else jumping and below jumpCeil
+            else
+            {
+                // Turn off jumping
+                isJumping = false;
+                // Gravity is applied
+                motion.Y += (gravity * (float)delta) * 2;
+            }
+        }// End if (isJumping)
     }
 
     private void HandleHorizontalAirMovement()
