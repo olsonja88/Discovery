1612d1bddc0fcb11578e5e4c21db53b6a373fc36
jaolson88 <jaolson190@gmail.com>
2023-04-17 14:01:28 -0500

    Refactor: Change timeElapsed to float, typecast delta to float, make minutes, seconds, and milleseconds floats as well.

--- stopwatch.cs
+++ stopwatch.cs
@@ -3,5 +3,16 @@ using System;
 
 public partial class stopwatch : Label
 {
+    float timeElapsed = 0.0f;
 
+    public override void _Process(double delta)
+    {
+        timeElapsed += (float)delta;
+
+        float minutes = timeElapsed / 60;
+        float seconds = timeElapsed % 60;
+        float milliseconds = (timeElapsed % 1) * 100;
+
+        // timeString = minutes + ":" + seconds + ":" + milliseconds;
+    }
 }
