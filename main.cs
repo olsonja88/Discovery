251ce889c70f7979b0359a217b9a762911912451
jaolson88 <jaolson190@gmail.com>
2023-04-19 13:54:41 -0500

    Fix: Add script to main node.  Create Respawn, Game Over, and New Game functions in main.cs

---
+++ main.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class main : Node
+{
+    public void RespawnPlayer()
+    {
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+
+    public void NewGame()
+    {
+
+    }
+}
